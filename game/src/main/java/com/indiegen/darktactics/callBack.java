99b0108999a5088c52b27ae1c1c22ad580475d90
Fernando De La Cruz Soto <fdelacruzsoto@gmail.com>
2017-06-22 21:30:40 -0500

    Change project structure

---
+++ game/src/main/java/com/indiegen/darktactics/callBack.java
@@ -0,0 +1,15 @@
+package com.indiegen.darktactics;
+
+public interface callBack
+{
+	
+	public void buttonExit();
+	public void buttonRestart();
+	public void buttonAttack();
+	public void buttonMove();
+
+
+	void buttonItem();
+
+	void buttonGuard();
+}
---
+++ game/src/main/java/com/indiegen/darktactics/game.java
@@ -0,0 +1,51 @@
+package com.indiegen.darktactics;
+import com.badlogic.gdx.*;
+import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.math.*;
+
+
+import com.badlogic.gdx.Game;
+
+public class game extends Game implements ApplicationListener
+{
+
+	Batch batch;
+	SpriteBatch spriteBatch;
+	screen screen1;
+	Assests assests;
+	
+	
+	public game()
+	{
+		//assests = new Assests();
+	}
+    
+	
+	@Override
+	public void create()
+	{
+		//assests = new Assests();
+		batch = new SpriteBatch();
+	    //world = new World(new Vector2(1,1),true);
+		screen1= new screen(this,batch);
+		setScreen(screen1);
+		
+		// TODO: Implement this method
+	}
+
+	@Override
+	public void dispose()
+	{
+		// TODO: Implement this method
+		//spriteBatch.dispose();
+		batch.dispose();
+		
+		super.dispose();
+		//screen1.dispose();
+		
+		
+		
+		
+	}
+	
+}
